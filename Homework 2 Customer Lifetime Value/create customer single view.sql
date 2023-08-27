8c4f2b4b32c22756ccd695886b772e70ce8283bf
ZeroGravigra <136248978+ZeroGravigra@users.noreply.github.com>
2023-08-28 01:26:08 +0700

    Update create customer single view.sql

--- Homework 2 Customer Lifetime Value/create customer single view.sql
+++ Homework 2 Customer Lifetime Value/create customer single view.sql
@@ -1,1 +1,12 @@
-
+SELECT 
+  Customer_ID, 
+  Segment,
+  min(Order_Date_parsed) as first_date , 
+  max(Order_Date_parsed) as last_date, 
+  date_diff(max(Order_Date_parsed), min(Order_Date_parsed), day) as life_span_days,
+  date_diff(date '2017-12-30', date (max(Order_Date_parsed)), day) as Recency,
+  date_diff(max(Order_Date_parsed), min(Order_Date_parsed), day)/count(distinct Order_ID) as MTBP,
+  count(distinct Order_ID) as no_order,
+  sum(Sales*Quantity) as total_sale, sum(Profit) as CLTV,
+FROM `madt-7102-final-project-dqcom.8101_CLTV.CLTV_Raw_Superstore` 
+group by Customer_ID, Segment
